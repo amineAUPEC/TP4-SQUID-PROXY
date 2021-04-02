aa7ce9058cf5200c5ba1ec28c3f9e153e201424b
amineAUPEC <79785242+amineAUPEC@users.noreply.github.com>
2021-04-02 11:40:33 +0200

    Update script25reset--.sh

--- tpfirewall/script25reset--.sh
+++ tpfirewall/script25reset--.sh
@@ -2,10 +2,9 @@
 cartes="eth0"
 echo "Reset de l’interface : $cartes" 
 sudo ip a flush dev $cartes &> /dev/null
-if [ $? -eq 0] # si la valeur de retour vaut 0 donc sans erreur 
+if [ $? -eq 0 ] # si la valeur de retour vaut 0 donc sans erreur 
     then 
         echo "OK" #donc on affiche ok
-else 
-    then 
+else  
     echo "NOK" #sinon on affiche NOK
 fi
