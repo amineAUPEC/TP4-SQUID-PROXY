fdf21f5cd726e8db1d51404397b2216d577f859a
amineAUPEC <79785242+amineAUPEC@users.noreply.github.com>
2021-04-02 10:52:20 +0200

    Create script25reset++.sh

---
+++ tpfirewall/script25reset++.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cartes="eth0 eth1 eth2"
+echo "Reset de l’interface : $cartes" 
+sudo ip a flush dev $cartes &> /dev/null
+if [ $? -eq 0] # si la valeur de retour vaut 0 donc sans erreur 
+    then 
+        echo "OK" #donc on affiche ok
+else 
+    then 
+    echo "NOK" #sinon on affiche NOK
+fi
